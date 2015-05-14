400415a1784de650b6703f5c18de6a3b5e5e42e3
Ken Todoroki <ken.todoroki@breachwalls.com>
2015-05-15 01:00:59 +0900

    add sidebar collapse and expand function.

--- src/main/webapp/scripts/components/sidebar/sidebar.directive.js
+++ src/main/webapp/scripts/components/sidebar/sidebar.directive.js
@@ -9,10 +9,21 @@ angular.module('majimenatestApp')
       scope: {
       },
       controller:function($scope){
+        $scope.minimum = false;
         $scope.selectedMenu = 'dashboard';
         $scope.collapseVar = 0;
         $scope.multiCollapseVar = 0;
-        
+
+        $scope.toggleSideMenu = function() {
+          // FIXME このへんはローカルストレージに設定を保存したほうが良さそう
+          $scope.minimum = !$scope.minimum;
+          if ($scope.minimum) {
+            $('#page-wrapper').addClass('sidebar-collapse');
+          } else {
+            $('#page-wrapper').removeClass('sidebar-collapse');
+          }
+        };
+
         $scope.check = function(x){
           
           if(x==$scope.collapseVar)
