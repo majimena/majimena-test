9ecdb444895725837fd51af91c426a42dd80e97d
Ken Todoroki <ken.todoroki@breachwalls.com>
2015-05-16 02:47:20 +0900

    sidebar menu and collapse sidebar function is finished.

--- src/main/webapp/scripts/components/sidebar/sidebar.directive.js
+++ src/main/webapp/scripts/components/sidebar/sidebar.directive.js
@@ -1,44 +1,46 @@
 'use strict';
 
 angular.module('majimenatestApp')
-  .directive('sidebar',['$location',function() {
-    return {
-      templateUrl:'scripts/components/sidebar/sidebar.html',
-      restrict: 'E',
-      replace: true,
-      scope: {
-      },
-      controller:function($scope){
-        $scope.minimum = false;
-        $scope.selectedMenu = 'dashboard';
-        $scope.collapseVar = 0;
-        $scope.multiCollapseVar = 0;
+    .directive('sidebar',['$location',function() {
+        return {
+            templateUrl:'scripts/components/sidebar/sidebar.html',
+            restrict: 'E',
+            replace: true,
+            scope: {
+            },
+            controller:function($scope) {
+                $scope.minimum = false;
+                $scope.selectedMenu = 'dashboard';
+                $scope.collapseVar = 0;
+                $scope.multiCollapseVar = 0;
 
-        $scope.toggleSideMenu = function() {
-          // FIXME このへんはローカルストレージに設定を保存したほうが良さそう
-          $scope.minimum = !$scope.minimum;
-          if ($scope.minimum) {
-            $('#page-wrapper').addClass('sidebar-collapse');
-          } else {
-            $('#page-wrapper').removeClass('sidebar-collapse');
-          }
-        };
+                $scope.toggleSideMenu = function() {
+                    // FIXME このへんはローカルストレージに設定を保存したほうが良さそう
+                    $scope.minimum = !$scope.minimum;
+                    if ($scope.minimum) {
+                        $('#page-wrapper').addClass('sidebar-collapse');
+                        $('#side-menu li ul').removeClass('nav-second-level');
+                    } else {
+                        $('#page-wrapper').removeClass('sidebar-collapse');
+                        $('#side-menu li ul').addClass('nav-second-level');
+                    }
+                };
 
-        $scope.check = function(x){
-          
-          if(x==$scope.collapseVar)
-            $scope.collapseVar = 0;
-          else
-            $scope.collapseVar = x;
+                $scope.check = function(x) {
+                    if (x === $scope.collapseVar) {
+                        $scope.collapseVar = 0;
+                    } else {
+                        $scope.collapseVar = x;
+                    }
+                };
+
+                $scope.multiCheck = function(y) {
+                    if (y === $scope.multiCollapseVar) {
+                        $scope.multiCollapseVar = 0;
+                    } else {
+                        $scope.multiCollapseVar = y;
+                    }
+                };
+            }
         };
-        
-        $scope.multiCheck = function(y){
-          
-          if(y==$scope.multiCollapseVar)
-            $scope.multiCollapseVar = 0;
-          else
-            $scope.multiCollapseVar = y;
-        };
-      }
-    }
-  }]);
+    }]);
